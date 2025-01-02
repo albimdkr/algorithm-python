aaea0d08df320fc70612720f59e39161c09646a3
albimdkr <albimudakar05@gmail.com>
2025-01-02 09:12:00 +0700

    add explaination

--- sliding_show.py
+++ sliding_show.py
@@ -42,4 +42,11 @@ rentang dalam variable nilai k=4 berarti kondisi memenuhi nya itersi nya sampai 
 - Iterasi 3: cari indeks array $nums ketiga[2] maka = 2 kemudian hitung $current_sum + 2 = 6
 - Iterasi 4: cari indeks array $nums keempat[3] maka = 5 berarti hitung $current_sum + 5 = 11
 Kemudian assign si current_sum nya menjadi max_sum = 11, kenapa 11? karena dari jumlah iterasi maks 4 kali dan hasil akhir nya 11
-'''
+Oke maka nilai maks nya ada 11 dan nilai rentang nya adalah 4 sampai 6
+Kita ambil kembali nilai dalam array sliding show adalah: 1,3,2,5,1,1,7
+Setelah itu, hitung nilai dengan menggeser nilai pada Geser jendela sepanjang array
+- Iterasi 1: cari indeks array $nums kelima[4] maka = 1 pada [i]terasi ke berapa[0] = k1 maka, kemudian hitung $current_sum = 11 + 1 - k1 = 11
+- Iterasi 2: cari indeks array $nums keeenam[5] maka = 1 pada [i]terasi ke berapa [1] = k3 maka, kemudian hitung $current_sum = 11 + 1 - k3 = 9
+- Iterasi 3: cari indeks array $nums ketujuh[6] maka = 7 pada [i]terasi ke berapa [2] = k2 kemudian hitung $current_sum = 9 + 7 - k2 = 14
+maka hasil dari nilai yang telah di hitung adalah = 14
+ '''
